bc7aefd82150591614bc4edd6454eee4b83a6dee
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-09-18 14:15:22 +0530

    lifting state up which are used in other component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
 import Player from "./components/Player.jsx"
 import GameBoard from "./components/GameBoard.jsx"
 function App() {
+  const [activeplayer, setActiveplayer] = useState('X')
+
+  function handleSelectSquare() {
+    setActiveplayer((currentplayer) => currentplayer === 'X' ? 'O' : 'X')
+  }
 
   return (
     <>
     <div id="game-container">
-      <ol id="players">
-        <Player name="Player 1" symbol="X" />
-        <Player name="Player 2" symbol="O" />
+      <ol id="players" className="highlight-player">
+        <Player name="Player 1" symbol="X" isActive={activeplayer === 'X'} />
+        <Player name="Player 2" symbol="O" isActive={activeplayer === 'O'} />
       </ol>
-      <GameBoard />
+      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activeplayer} />
     </div>
     Score will be here
     </>
