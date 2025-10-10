a08f84098395101b6c3c501eab42227762f8959a
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-10-10 19:21:09 +0530

    add component in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import TicTacToe from './components/Tic-Tac-Toe Game/TicTacToe.jsx';
 import PausePlay from './components/Pause & Play/PausePlay.jsx';
+import ReactQuiz from './components/Quiz/ReactQuiz.jsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <nav>
         <Link to="/tic-tac-toe" style={{ margin: "0 1rem" }}>Tic Tac Toe</Link>
         <Link to="/play-pause" style={{ margin: "0 1rem" }}>Play & Pause</Link>
+        <Link to="/quiz" style={{ margin: "0 1rem" }}>Quiz</Link>
       </nav>
 
       <Routes>
@@ -15,6 +17,7 @@ function App() {
 
         <Route path="/tic-tac-toe" element={<TicTacToe />} />
         <Route path="/play-pause" element={<PausePlay />} />
+        <Route path="/quiz" element={<ReactQuiz />} />
 
         <Route path="*" element={<h2 className="fallback">Select a game above</h2>} />
       </Routes>
