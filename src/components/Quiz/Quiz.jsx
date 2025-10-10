89890241779ca54bee4fdd0b8a3457a042581f41
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-10-10 14:06:08 +0530

    quiz completed dom added

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -4,12 +4,27 @@ import QUESTIONS from './questions.js'
 export default function Quiz() {    
    const [userAnswer, setUserAnswer] = useState([])
    const activeQuestion = userAnswer.length
+   const quizComplete = activeQuestion === QUESTIONS.length
 
    function handleSelectAnswer(selectedAnswer){
       setUserAnswer((prevAnswers) => {
         return [...prevAnswers, selectedAnswer]
       })
    }
+
+   if(quizComplete){
+    return (
+        <div className={styles.summary}>
+             <img
+                    src="/quiz-complete.png"
+                    alt="Trophy icon" />
+                  <h2 className={styles.h2}>Game Over</h2>
+        </div>
+    )
+   }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestion].answers]
+   shuffledAnswers.sort(() => Math.random() - 0.5)
    
   return (
    <div className={styles.quiz}>
@@ -17,7 +32,7 @@ export default function Quiz() {
        <h2>{QUESTIONS[activeQuestion].text}</h2>
    </div>
    <ul className={styles.answers}>
-         {QUESTIONS[activeQuestion].answers.map((answer) => (
+         {shuffledAnswers.map((answer) => (
             <li key={answer} className={styles.answer}>
                 <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
